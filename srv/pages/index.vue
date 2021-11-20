ea88e7b7bffea303be38e01fe4b1999814c16231
Nakusu <leopold.lepage@gmail.com>
2021-11-20 21:28:46 +0100

    get client adress

--- srv/pages/index.vue
+++ srv/pages/index.vue
@@ -1,3 +1,55 @@
 <template>
-    <h1>Welcome !</h1>
+  <div class="content-home">
+    <div v-if="!client" class="block">
+      <h1>You have accept my lord !</h1>
+    </div>
+    <div v-if="client != null" class="block">
+        <h1>Welcome my lord !</h1>
+        <p>Your adress : {{ adress }}</p>
+    </div>
+  </div>
 </template>
+
+<style>
+    html {
+        background-color: black;
+        color: white;
+    }
+</style>
+
+<script>
+const Web3 = require("web3");
+
+export default {
+  data() {
+    return {
+      web3: new Web3("https://cloudflare-eth.com"),
+      client: null,
+      adress: null,
+    };
+  },
+  methods: {
+    async getBlockNumber() {
+      const latestBlockNumber = await this.web3.eth.getBlockNumber();
+      console.log(latestBlockNumber);
+    },
+    async getAccess() {
+      if (window.ethereum != null) {
+        if (window.web3) {
+          this.client = new Web3(web3.currentProvider);
+          this.adress = this.client.currentProvider.selectedAddress;
+          this.network = this.currentProvider.network;
+          return;
+        }
+        try {
+          await window.ethereum.enable();
+          this.client = new Web3(window.ethereum);
+        } catch (error) {}
+      }
+    },
+  },
+  async mounted() {
+    this.getAccess();
+  },
+};
+</script>
